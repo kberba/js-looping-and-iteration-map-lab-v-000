0e090355e0e8c161c3aad666d30d553dc2b76728
Karen Berba <kberba@gmail.com>
2018-03-04 02:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function lowerCaseDrivers(drivers) {
 function nameToAttributes(drivers) {
   // let fullName = drivers.map(function (aDriver) {
   //   return Object.assign({}, aDriver, { firstName: aDriver.firstName, lastName: aDriver.lastName });
-  let fullName = drivers.map(function (aDriver) {
+  return drivers.map(function (aDriver) {
     let splitName = aDriver.split(" ");
     return Object.assign({}, { firstName: splitName[0]}, { lastName: splitName[1]});
   };
-  return fullName;
+
 }
