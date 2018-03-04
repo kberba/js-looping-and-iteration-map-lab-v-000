c9fab53fae2f429f59da5dd0c00c729216943765
Karen Berba <kberba@gmail.com>
2018-03-04 01:54:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   let fullName = map(drivers, function (aDriver) {
-    return Object.assign({}, aDriver, { firstName: drivers.firstName, lastName: drivers.lastName });
+    return Object.assign({}, aDriver, { firstName: aDriver.firstName, lastName: aDriver.lastName });
 }
 
  // const newEngineers = map(oldAccounts, function (account) {
