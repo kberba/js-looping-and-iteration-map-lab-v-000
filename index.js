dca77396d012c9ab90265216735b20f171afb6f1
Karen Berba <kberba@gmail.com>
2018-03-04 02:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ function lowerCaseDrivers(drivers) {
   return lowCaseD;
 }
 
-function nameToAttributes(drivers) {
-  // let fullName = drivers.map(function (aDriver) {
-  //   return Object.assign({}, aDriver, { firstName: aDriver.firstName, lastName: aDriver.lastName });
-  return drivers.map(function(aDriver) {
-    let splitName = aDriver.split(" ");
-    return Object.assign({}, { firstName: splitName[0]}, { lastName: splitName[1]});
-  };
+// function nameToAttributes(drivers) {
+//   // let fullName = drivers.map(function (aDriver) {
+//   //   return Object.assign({}, aDriver, { firstName: aDriver.firstName, lastName: aDriver.lastName });
+//   return drivers.map(function(aDriver) {
+//     let splitName = aDriver.split(" ");
+//     return Object.assign({}, { firstName: splitName[0]}, { lastName: splitName[1]});
+//   };
+// }
+
+function nameToAttributes(drivers){
+  return drivers.map(function(element){
+    let nameArray = element.split(" ");
+    return Object.assign({},
+    {firstName: nameArray[0]},
+    {lastName: nameArray[1]}
+    );
+  });
 }
