a3e3f769ce645645878b48b0cdf6c2d01c2d961f
Karen Berba <kberba@gmail.com>
2018-03-04 02:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-   return drivers.map(function(aDriver) {
-     let splitName = aDriver.split(" ");
-     return Object.assign({},
-       {firstName: splitName[0]},
-       {lastName: splitName[1]}
-     );
-   });
+  //  return drivers.map(function(aDriver) {
+  //    let splitName = aDriver.split(" ");
+  //    return Object.assign({},
+  //      {firstName: splitName[0]},
+  //      {lastName: splitName[1]}
+  //    );
+  //  });
+  return drivers.map(aDriver => {
+    let splitName = aDriver.split(" ");
+    return Object.assign({},
+      {firstName: splitName[0]},
+      {lastName: splitName[1]}
+    );
+  });
  }
 
  function attributesToPhrase(drivers) {
