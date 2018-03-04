34c3b37c1afc31f9f7310cce98433def92b4cb3e
Karen Berba <kberba@gmail.com>
2018-03-04 01:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-   let fullName = drivers.map(function (aDriver) {
-     return Object.assign({}, aDriver, { firstName: aDriver.firstName, lastName: aDriver.lastName });
+  // let fullName = drivers.map(function (aDriver) {
+  //   return Object.assign({}, aDriver, { firstName: aDriver.firstName, lastName: aDriver.lastName });
+  let fullName = drivers.map(driv => {
+    let rName = {};
+    rName[driv.key] = driv.value;
+    return rName;
+  })
 }
 
- // const newEngineers = map(oldAccounts, function (account) {
- //   return Object.assign({}, account, { accessLevel: 'admin' });
- // });
+var kvArray = [{key: 1, value: 10},
+               {key: 2, value: 20},
+               {key: 3, value: 30}];
+
+var reformattedArray = kvArray.map(obj =>{
+   var rObj = {};
+   rObj[obj.key] = obj.value;
+   return rObj;
+})
+// reformattedArray is now [{1: 10}, {2: 20}, {3: 30}],
