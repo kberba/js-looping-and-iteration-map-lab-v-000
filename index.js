2a57ff19cdbd911f2622d9459eaa307131f06c6b
Karen Berba <kberba@gmail.com>
2018-03-04 02:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function lowerCaseDrivers(drivers) {
 function nameToAttributes(drivers) {
   // let fullName = drivers.map(function (aDriver) {
   //   return Object.assign({}, aDriver, { firstName: aDriver.firstName, lastName: aDriver.lastName });
+  let splitName = drivers.split(" ");
+  const fullName = return Object.assign({}, { firstName: splitName[0], lastName: splitName[1]});
 }
+
+
+const old = [
+  {name: 'a', value: 'b', other: 'c'}, 
+  {name: 'd', value: 'e', other: 'f'}
+]
+
+const transformed = Object.assign(
+  {}, 
+  ...old.map(({name, value}) => ({ [name]: value }))
+);
+
+console.log(transformed);
